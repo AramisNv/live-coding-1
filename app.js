d5448da032d4ecb7fdad164ec1056c4abfb7957f
Aramis Nava <ara_nmc@hotmail.com>
2023-06-22 14:53:11 -0700

    Se sube ejercicio tratando de hacer una funciones de arrays y dom

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// Escribe tu código aquí.
+
+function parametros (){
+   let parametro1  = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio']
+   let parametro2 = "String" ;{
+    console.log(parametro1 , parametro2)
+    console.log (parametro1.length);
+    console.log (parametro2.length);
+   }
+   for (i = 0; i<parametro1.length; i++){
+    parametro1[i]; console.log(parametro1 [i]);
+   if (parametro1[i].length> parametro2.length ) {
+    console.log("Estas palabras tienen mas letras que mi parametro2:" , parametro1[i]);
+   }
+ } 
+
+   }
+
+   parametros();
+
+
+
